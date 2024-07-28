ce7e956484fc634fe3a412c045a29c32f9316bea
FrozenCell239 <val8ioks@gmail.com>
2024-07-28 19:08:03 +0200

    Feature addition - Added a contact form that allow any user to send a suggestion or a support request.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
 use App\Repository\PostRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController {
@@ -19,6 +23,52 @@ class MainController extends AbstractController {
             };
         };
         return $this->render('main/index.html.twig', compact('posts'));
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(
+        Request $request,
+        MailerInterface $mailer
+    ) : Response {
+        # Creating the contact form
+        $contact_form = $this->createForm(ContactFormType::class);
+        $contact_form->handleRequest($request);
+
+        # Checking if the form is submitted and is valid
+        if($contact_form->isSubmitted() && $contact_form->isValid()){
+            # Creating the email
+            $help_seeker_address = $contact_form->get('email')->getData();
+            $email = (new TemplatedEmail())
+                ->from(new Address($help_seeker_address))
+                ->to($this->getParameter('dev_email'))
+                ->subject("Suggestion/demande d'assistance")
+                ->htmlTemplate('emails/support_request_email.html.twig')
+                ->context([
+                    'help_seeker_address' => $help_seeker_address,
+                    'message' => $contact_form->get('message')->getData()
+                ])
+            ;
+
+            # Attempting to sending the email
+            try{
+                $mailer->send($email);
+            }
+            catch(\Throwable $th){
+                $this->addFlash(
+                    'error',
+                    "Une erreur est survenue lors de l'envoi du message. Veuillez réessayer ultérieurement."
+                );
+                return $this->render('main/contact.html.twig', compact('contact_form'));
+            };
+            $this->addFlash(
+                'success',
+                "Votre message a été envoyé avec succès ! Notre équipe le traîtera dans les plus brefs délais."
+            );
+            return $this->redirectToRoute('app_index');
+        };
+
+        # Displaying the form
+        return $this->render('main/contact.html.twig', compact('contact_form'));
     }
 
     #[Route(
